0c2519fa2b4962a0af16b3d674472a68742816dc
osamashehta <osama.shehtafathi@gmail.com>
2025-01-23 11:16:48 +0200

    update

--- src/Component/Navbar/Header.jsx
+++ src/Component/Navbar/Header.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { Link } from "react-router-dom";
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
   useEffect(() => {
     const theme = localStorage.getItem("theme");
     if (theme === "dark") {
@@ -65,6 +67,7 @@ function Header() {
 
         <button
           data-collapse-toggle="navbar-dropdown"
+          onClick={()=> setMenuOpen(!menuOpen)}
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-dropdown"
